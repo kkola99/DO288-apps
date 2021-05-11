05673667daee126bf5387558d2fdc989711777f6
kkola99 <83407366+kkola99@users.noreply.github.com>
2021-05-11 18:30:07 +0300

    Update server.js

--- day7/node/server.js
+++ day7/node/server.js
@@ -7,6 +7,10 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
+ host: process.env.host,
+ database: process.env.database,
+ user: process.env.user,
+ password: process.env.password
 });
 con.connect(function(err) {
  if (err) throw err;
